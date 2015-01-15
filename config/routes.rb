e2adbcc9fcbae93f8c9462c95b47751740f67422
muratdbc <murat.dbc@gmail.com>
2015-01-14 22:24:01 -0800

    added user model and tested api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 require 'api_constraints'
 
 Rentlever::Application.routes.draw do
+  devise_for :users
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1,default: :true) do
       resources :units
+      resources :users
     end
   end
 end
